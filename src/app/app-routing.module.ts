720d9ae07361e09a02336c4ece0c70b5c0e6a329
Huey Ching Poh <e0231960@u.nus.edu>
2018-02-08 14:58:58 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';     // Add this for routing
+import { AboutComponent } from './about/about.component';  // Add this for routing
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { 
+  path:'',
+  component: HomeComponent
+  },
+
+  { 
+    //id is route parameter
+    path:'about/:id',
+    component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+//component decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+//this is where all the logic resides
 export class AppComponent {
   title = 'app';
 }
